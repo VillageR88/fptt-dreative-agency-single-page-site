7a2071f12da2056ed17ac7284227464ee5aa53bc
VillageR88 <villager88pl@gmail.com>
2024-08-11 19:15:27 +0200

    navbar initial
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/static/pages/components/Navbar.tsx
+++ app/static/pages/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import logo from '../../assets/desktop/logo.svg';
 import { Link } from 'preact-router';
 
 const items = [
@@ -15,18 +16,28 @@ const items = [
   },
 ];
 
+const buttonTitle = 'Schedule a Call';
+
 export default function Navbar() {
   return (
     <nav className="z-[2] flex h-[64px] w-full items-center justify-between overflow-x-clip px-[32px] md:h-[178px] md:px-[40px]">
-      <div>LOGO</div>
-      <div className="flex h-full w-[705px] items-center bg-[#F94F4F] pl-[69px]">
-        <ul className="flex gap-[32px] text-white">
+      <img src={logo as string} alt="logo" />
+      <div className="flex h-full w-[705px] items-center gap-[48px] bg-[#F94F4F] pl-[69px]">
+        <ul className="flex gap-[32px]">
           {items.map((item) => (
             <li key={item.title}>
-              <Link href={item.href}>{item.title}</Link>
+              <Link className="text-[16px] leading-[32px] text-white transition-all hover:font-bold" href={item.href}>
+                {item.title}
+              </Link>
             </li>
           ))}
         </ul>
+        <button
+          type="button"
+          className="h-[64px] w-[199px] bg-[#191921] text-[16px] font-extrabold text-white transition hover:bg-[#434356]"
+        >
+          {buttonTitle}
+        </button>
       </div>
     </nav>
   );
