b0d4133d21f1384972f0ab4d4e4fe25945792d6f
VillageR88 <villager88pl@gmail.com>
2024-08-12 23:11:05 +0200

    mobile menu
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/static/pages/components/Navbar.tsx
+++ app/static/pages/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'preact/hooks';
 import logo from '../../assets/desktop/logo.svg';
 import { Link } from 'preact-router';
 
@@ -19,9 +20,36 @@ const items = [
 const buttonTitle = 'Schedule a Call';
 
 export default function Navbar() {
+  const menuButtonRef = useRef<HTMLButtonElement>(null);
+  function handleOpen() {
+    if (menuButtonRef.current) {
+      menuButtonRef.current.classList.toggle('open');
+    }
+  }
   return (
     <nav className="relative z-[2] flex h-[110px] w-full max-w-[calc(1275px+39px)] items-center justify-between self-end overflow-x-clip pl-[39px] md:h-[178px]">
-      <img src={logo as string} alt="logo" />
+      <div id="mask" className="fixed left-0 top-0 z-[1] flex size-full" />
+      <div
+        id="mobileMenu"
+        className="absolute right-[26px] top-[110px] z-40 flex h-[292px] w-full max-w-[252px] flex-col items-center gap-[24px] bg-black pt-[32px]"
+      >
+        <ul className=" relative z-40 flex flex-col items-center gap-[24px]">
+          {items.map((item) => (
+            <li className="relative z-40" key={item.title}>
+              <Link className=" relative z-40 text-[18px] leading-[32px] text-white transition-all" href={item.href}>
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <button
+          type="button"
+          className="relative z-40 h-[64px] w-[199px] bg-[#F94F4F] text-[16px] font-extrabold text-white"
+        >
+          {buttonTitle}
+        </button>
+      </div>
+      <img className="relative -z-0 flex" src={logo as string} alt="logo" />
       <div className="hidden size-full max-w-[438px] items-center gap-[28px] bg-[#F94F4F] pl-[33px] pr-[40px] md:flex lg:max-w-[530px] lg:gap-[50px] lg:pr-[20px] xl:max-w-[47vw] xl:gap-[48px] xl:pl-[69px] xl:pr-0 screen1500:max-w-[705px]">
         <ul className="flex w-full max-w-[190px] justify-between xl:max-w-[225px]">
           {items.map((item) => (
@@ -39,7 +67,16 @@ export default function Navbar() {
           {buttonTitle}
         </button>
       </div>
-      <button id="mobileNav" title="mobile navigation" type="button" className="absolute right-[24px] md:opacity-0" />
+      <button
+        ref={menuButtonRef}
+        onClick={() => {
+          handleOpen();
+        }}
+        id="mobileNav"
+        title="mobile navigation"
+        type="button"
+        className="absolute right-[24px] z-[2] md:opacity-0"
+      />
     </nav>
   );
 }
