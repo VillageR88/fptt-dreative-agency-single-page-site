74b7f8c809c6ac8fa9e2221fb81af74a4b38930f
VillageR88 <villager88pl@gmail.com>
2024-08-13 09:34:29 +0200

    footer all screens
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/static/pages/components/Footer.tsx
+++ app/static/pages/components/Footer.tsx
@@ -7,7 +7,7 @@ const items = {
 
 export default function Footer() {
   return (
-    <footer className="mx-auto mb-[101px] mt-[70px] flex w-full max-w-[1342px] items-center px-[40px] xl:mb-[115px] xl:ml-[-67px] xl:mt-[120px] xl:px-0">
+    <footer className="mx-auto mb-[101px] mt-[100px] flex w-full max-w-[1342px] items-center overflow-hidden px-[24px] md:mt-[70px] md:px-[40px] xl:mb-[115px] xl:ml-[-67px] xl:mt-[120px] xl:px-0">
       <img className="hidden xl:block" alt="pattern" src={imagePattern as string} width={134} height={60} />
       <div className="flex w-full flex-col justify-between gap-[47px] md:flex-row md:items-center xl:ml-[98px]">
         <h2 className="md:max-w-[398px] xl:max-w-[635px]">{items.title}</h2>
--- app/static/pages/layout.tsx
+++ app/static/pages/layout.tsx
@@ -6,14 +6,14 @@ import Footer from './components/Footer';
 import './globals.css';
 
 document.body.className =
-  'min-h-dvh mx-auto md:min-h-screen max-w-[1440px] flex flex-col items-center font-commissioner justify-space-between';
+  'min-h-dvh mx-auto md:min-h-screen max-w-[1440px]  flex flex-col items-center font-commissioner justify-space-between';
 
 function App() {
   return (
     <>
       {/* <DataContext> */}
       <Navbar />
-      <main className="mx-auto mb-auto flex w-full flex-col items-center overflow-x-clip">
+      <main className="mx-auto mb-auto flex  w-full flex-col items-center overflow-x-clip">
         <Home />
       </main>
       <Footer />
